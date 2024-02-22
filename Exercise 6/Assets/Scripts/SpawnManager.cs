3924ede491551cdb464d5cfe955824f028b7217e
Aleck Hernandez <73125960+AleckHernandez@users.noreply.github.com>
2024-02-22 11:46:27 -0500

    Random position and color

--- Exercise 6/Assets/Scripts/SpawnManager.cs
+++ Exercise 6/Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public enum AnimalTypes
 {
     Elephant, 
-    Turtle,
-    Snail,
+    Kangaroo,
     Octopus,
-    Kangaroo
+    Snail,
+    Turtle
 }
 
 
@@ -23,6 +23,11 @@ public class SpawnManager : Singleton<SpawnManager>
 
     public SpriteRenderer animalPrefab;
 
+    [SerializeField]
+    private int animalCount;
+
+    public List<SpriteRenderer> spawnedAnimals;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,5 +38,59 @@ public class SpawnManager : Singleton<SpawnManager>
     void Update()
     {
         
+    }
+
+    private float Gaussian(float mean, float stdDev)
+    {
+        float val1 = Random.Range(0f, 1f);
+        float val2 = Random.Range(0f, 1f);
+
+        float gaussValue =
+        Mathf.Sqrt(-2.0f * Mathf.Log(val1)) *
+        Mathf.Sin(2.0f * Mathf.PI * val2);
+
+        return mean + stdDev * gaussValue;
+    }
+
+
+    public void Spawn()
+    {
+        CleanUp();
+        for(int i = 0; i < animalCount; i++)
+        {
+            SpawnAnimal();
+        }
+    }
+
+    public void SpawnAnimal()
+    {
+        SpriteRenderer newAnimal = Instantiate(animalPrefab);
+
+        // Change sprite
+        newAnimal.sprite = sprites[(int)AnimalTypes.Elephant];
+
+        // Change color
+        newAnimal.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+        // Set position
+        float screenH = Camera.main.orthographicSize * 2f;
+        float screenW = screenH * Camera.main.aspect;
+
+        float x = Gaussian(0, screenW / 8);
+        float y = Gaussian(0, screenH / 8);
+
+        newAnimal.transform.position = new Vector3(x, y, 0);
+        
+
+        spawnedAnimals.Add(newAnimal);
+    }
+
+    void CleanUp()
+    {
+        foreach(SpriteRenderer animal in spawnedAnimals)
+        {
+            Destroy(animal.gameObject);
+        }
+        spawnedAnimals.Clear();
     }
 }
