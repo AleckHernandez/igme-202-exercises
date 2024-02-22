f1e886a4fc2d78b148a67fadeb95f145d9b87ad1
Aleck Hernandez <73125960+AleckHernandez@users.noreply.github.com>
2024-02-22 11:12:19 -0500

    enum

--- Exercise 6/Assets/Scripts/SpawnManager.cs
+++ Exercise 6/Assets/Scripts/SpawnManager.cs
@@ -2,6 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum AnimalTypes
+{
+    Elephant, 
+    Turtle,
+    Snail,
+    Octopus,
+    Kangaroo
+}
+
+
 public class SpawnManager : Singleton<SpawnManager>
 {
     // (Optional) Prevent non-singleton constructor use.
@@ -9,6 +20,8 @@ public class SpawnManager : Singleton<SpawnManager>
 
     [SerializeField]
     private List<Sprite> sprites;
+
+    public SpriteRenderer animalPrefab;
 
     // Start is called before the first frame update
     void Start()
